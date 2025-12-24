9104c66fb689ccdf2cc7d76830f622feb01b55e3
eugenii <eugenii.cher@gmail.com>
2025-12-24 08:40:24 +0300

    start.js variables

--- simple_electric_stand/start.js
+++ simple_electric_stand/start.js
@@ -4,3 +4,12 @@ let U0_values = [0, 1.5, 3, 4.5, 6, 9, 12, 27];
 let U0 = U0_values[2];
 let Rp_values = [0, 5, 10, 15, 20, 25];
 let Rp = Rp_values[2];
+let U1 = 0;
+let I0 = 0;
+let Ra = 5;
+let Rb = 10;
+let Rc = 15;
+let Rd = 20;
+let R_ = 0;
+
+let key = false;
